727dc53bae29060ec900ef9a5cf5f8fd3a0e2d0a
yasuyasu-ot <y.otsubo.dss@gmail.com>
2022-07-27 07:03:13 +0900

    helloパスで文字列を返却

--- helloworldproject/helloworldapp/views.py
+++ helloworldproject/helloworldapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse # モジュールの読み込み
 
 # Create your views here.
+def hellofunction(request):
+    return HttpResponse('Hello World!')
